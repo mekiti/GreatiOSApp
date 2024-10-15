e4c959aa2dc75705144c79589fc8afe29e4d8ddc
Domas Zaboras <domas@trendsales.dk>
2024-10-15 14:10:10 +0300

    add loading state

--- GreatiOSApp/Constants/Constants.swift
+++ GreatiOSApp/Constants/Constants.swift
@@ -4,6 +4,7 @@ struct Constants {
     static let loginString = "Log in"
     static let usernameString = "Username"
     static let passwordString = "Password"
+    static let loadingString = "Loading list"
     static let loginPadding = CGFloat(32)
     static let cornerRadius = CGFloat(10)
     static let texfieldIconSize = CGFloat(14)
--- GreatiOSApp/Views/LoginView.swift
+++ GreatiOSApp/Views/LoginView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var isLoading: Bool = false
+
     var body: some View {
         ZStack {
             Image(.background)
@@ -9,24 +11,33 @@ struct LoginView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
                 .ignoresSafeArea()
 
-            VStack(spacing: 0) {
-                Image(.logo)
-                    .padding(.bottom, Constants.imagePadding)
-                EntryFieldView(placeHolder: Constants.usernameString, icon: .userIcon)
-                    .padding(.bottom, Constants.entryFieldPadding)
-                EntryFieldView(placeHolder: Constants.passwordString, icon: .lockIcon, isSecure: true)
-                    .padding(.bottom, Constants.buttonPadding)
-                Button {
-
-                } label: {
-                    Text(Constants.loginString)
-                        .frame(maxWidth: .infinity)
+            if isLoading {
+                VStack(spacing: 8) {
+                    ProgressView()
+                        .font(.footnote)
+                    Text(Constants.loadingString)
+                        .foregroundStyle(.loadingText)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.button)
+            } else {
+                VStack(spacing: 0) {
+                    Image(.logo)
+                        .padding(.bottom, Constants.imagePadding)
+                    EntryFieldView(placeHolder: Constants.usernameString, icon: .userIcon)
+                        .padding(.bottom, Constants.entryFieldPadding)
+                    EntryFieldView(placeHolder: Constants.passwordString, icon: .lockIcon, isSecure: true)
+                        .padding(.bottom, Constants.buttonPadding)
+                    Button {
+                        isLoading = true
+                    } label: {
+                        Text(Constants.loginString)
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.button)
+                }
+                .padding(Constants.loginPadding)
+                .ignoresSafeArea(.keyboard, edges: .bottom)
             }
-            .padding(Constants.loginPadding)
-            .ignoresSafeArea(.keyboard, edges: .bottom)
         }
     }
 }
