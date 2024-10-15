d381ee69af951805973da6ca0d817f6c590c71b8
Domas Zaboras <domas@trendsales.dk>
2024-10-15 11:25:20 +0300

    Init project

---
+++ GreatiOSApp/GreatiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatiOSAppApp.swift
+//  GreatiOSApp
+//
+//  Created by Domas Zaboras on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreatiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
